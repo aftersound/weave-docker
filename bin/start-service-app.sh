bd1fef09212e7f8a4cb4630365a4646eba40bd76
aftersound <aftersound@gmail.com>
2021-03-28 19:32:20 -0700

    update bin/start-service-app.sh
    change to use docker env APPLICATION instead of NAMESPACE

--- bin/start-service-app.sh
+++ bin/start-service-app.sh
@@ -25,4 +25,4 @@ for file in $weavehome/service/library/extra/*; do
   fi
 done
 
-java -DWEAVE_HOME=$weavehome -XX:TieredStopAtLevel=1 -XX:TieredStopAtLevel=1 -noverify -Dspring.output.ansi.enabled=always -Dcom.sun.management.jmxremote -Dspring.jmx.enabled=true -Dspring.liveBeansView.mbeanDomain -Dspring.application.admin.enabled=true -Dfile.encoding=UTF-8 -classpath "$classpath" -Dspring.config.location=$weavehome/service/$NAMESPACE.properties io.aftersound.weave.service.Application
+java -DWEAVE_HOME=$weavehome -XX:TieredStopAtLevel=1 -XX:TieredStopAtLevel=1 -noverify -Dspring.output.ansi.enabled=always -Dcom.sun.management.jmxremote -Dspring.jmx.enabled=true -Dspring.liveBeansView.mbeanDomain -Dspring.application.admin.enabled=true -Dfile.encoding=UTF-8 -classpath "$classpath" -Dspring.config.location=$weavehome/service/$APPLICATION.properties io.aftersound.weave.service.Application
